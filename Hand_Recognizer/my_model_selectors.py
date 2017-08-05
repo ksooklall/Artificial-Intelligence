09d3a948a3990a59913458bd8e56c1e2612913db
ksooklall <kensprogramming42@gmail.com>
2017-08-05 18:14:36 -0400

    completed BIC DIC and CV models

--- Hand_Recognizer/my_model_selectors.py
+++ Hand_Recognizer/my_model_selectors.py
@@ -70,14 +70,29 @@ class SelectorBIC(ModelSelector):
 
     def select(self):
         """ select the best model for self.this_word based on
-        BIC score for n between self.min_n_components and self.max_n_components
+        BIC score for n between self.min_comp and self.max_comp
 
         :return: GaussianHMM object
         """
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-        # TODO implement model selection based on BIC scores
-        raise NotImplementedError
+        BIC_model = None
+        BIC_score = float('inf')
+
+        for comp in range(self.min_n_components, self.max_n_components + 1):
+            try:
+                model = self.base_model(comp)
+                n_params = (comp ** 2) + 2 * len(self.X[0]) * comp
+                logL = model.score(self.X, self.lengths)
+                N = np.sum(self.lengths)
+                current_score = -2 * logL + n_params * np.log(N)
+                if current_score < BIC_score:
+                    BIC_score = current_score
+                    BIC_model = model
+            except:
+                pass
+
+        return BIC_model
 
 
 class SelectorDIC(ModelSelector):
@@ -92,8 +107,32 @@ class SelectorDIC(ModelSelector):
     def select(self):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-        # TODO implement model selection based on DIC scores
-        raise NotImplementedError
+        DIC_score = -float("inf")
+        DIC_model = None
+        
+        for comp in range(self.min_n_components, self.max_n_components + 1):
+            temp_score = 0
+            try:
+                model = self.base_model(comp)
+                score = model.score(self.X, self.lengths) 
+                words = self.words.copy()
+                
+                del words[self.this_word]
+                
+                for word in words:
+                    otherX, otherlength = self.hwords[word]
+                    try:
+                        temp_score += model.score(otherX, otherlength)
+                    except:
+                        pass
+                temp_score = temp_score / len(words)
+                current_score = score - temp_score
+                if current_score > DIC_score:
+                    DIC_score = current_score
+                    DIC_model = model
+            except:
+                pass
+        return DIC_model
 
 
 class SelectorCV(ModelSelector):
@@ -104,5 +143,33 @@ class SelectorCV(ModelSelector):
     def select(self):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-        # TODO implement model selection using CV
-        raise NotImplementedError
+        best_score = float('-inf')
+        best_num_state = 1
+
+        for state in range(self.min_n_components, self.max_n_components + 1):
+            i = 0
+            score = 0
+            word_sequences = self.sequences
+            try:
+                CV_model = KFold(n_splits=min(3, len(word_sequences)))
+            except:
+                return None
+
+            try:
+                for train, test in CV_model.split(word_sequences):
+                    train_x, train_len = combine_sequences(train, word_sequences)
+                    test_x, test_len = combine_sequences(test, word_sequences)
+
+                    model = GaussianHMM(n_components=state, covariance_type='diag', n_iter=n, random_state=self.random_state,
+                                        verbose=False)
+                    model.fit(train_x, train_len)
+                    score += model.score(test_x, test_len)
+                    i += 1
+
+                if score/i > best_score:
+                    best_score = score / i
+                    best_num_state = state
+            except:
+                pass
+
+        return self.base_model(best_num_state)
