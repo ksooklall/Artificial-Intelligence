5997c090f6bc7e0a882c820afa5f803c0a9a2334
ksooklall <kensprogramming42@gmail.com>
2017-09-26 20:41:23 -0400

    finished first part

--- my_answers.py
+++ my_answers.py
@@ -13,12 +13,16 @@ def window_transform_series(series, window_size):
     X = []
     y = []
 
-    # reshape each 
+    #import pdb; pdb.set_trace()
+    for i in range(len(series) - window_size):
+        X.append(series[i:i+window_size])
+        y.append(series[i + window_size])
+
+    # reshape each
     X = np.asarray(X)
     X.shape = (np.shape(X)[0:2])
     y = np.asarray(y)
     y.shape = (len(y),1)
-
     return X,y
 
 # TODO: build an RNN to perform regression on our time series input/output data
@@ -29,8 +33,7 @@ def build_part1_RNN(window_size):
 ### TODO: return the text input with only ascii lowercase and the punctuation given below included.
 def cleaned_text(text):
     punctuation = ['!', ',', '.', ':', ';', '?']
-
-    return text
+    return text.lower()
 
 ### TODO: fill out the function below that transforms the input text and window-size into a set of input/output pairs for use with our RNN model
 def window_transform_text(text, window_size, step_size):
